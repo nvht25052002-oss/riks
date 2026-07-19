40fec8e4a21c0d5bbd3f0ca46b5365d9e5025aaa
nvht25052002-oss <nvht25052002@gmail.com>
2026-07-20 04:52:55 +0700

    init: home page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState, useRef, useEffect } from 'react'
+import Chat from '@/components/Chat'
+
+export default function Home() {
+  return (
+    <main>
+      <Chat />
+    </main>
+  )
+}
